414a46f0cce29e8fc33be0cbe81974e3fc031269
JasonChavarria07 <108899160+JasonChavarria07@users.noreply.github.com>
2025-04-01 21:33:44 -0600

    fefe

---
+++ HDOpticasJAVS/Controllers/PuntoVentaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HDOpticasJAVS.Controllers
+{
+    public class PuntoVentaController : Controller
+    {
+        // GET: PuntoVenta
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
