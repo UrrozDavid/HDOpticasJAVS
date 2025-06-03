9c518b2c3369cc37cd6809c6bff0ba55ea562d93
Steph0328 <smontero98@gmail.com>
2025-06-02 22:38:32 -0600

    Se agregan las opciones de notas de credito y reportes

---
+++ HDOpticasJAVS/Models/ReporteVenta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HDOpticasJAVS.Models
+{
+    public class ReporteVenta
+    {
+        public int Id_Venta { get; set; }
+        public DateTime Fecha_Venta { get; set; }
+        public string MetodoPago { get; set; }
+        public decimal Total { get; set; }
+        public string Empleado { get; set; }
+    }
+}
